7bc74f99c526abf5784b0cf72ba88a0ed4917601
shushilgirish <girish.sh@northeastern.edu>
2023-10-21 14:06:07 -0400

    Professor and Employer Updates

--- Aed Project/src/Employer/EmployerProfile.java
+++ Aed Project/src/Employer/EmployerProfile.java
@@ -2,17 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Professor;
+package Employer;
 
 /**
  *
  * @author shush
  */
-public class ProfessorProfile {
-    String Prof_first_name;
-    String Prof_last_name;
-    String Prof_mail;
-    String Prof_id;
-    String Prof_speciality;
+public class EmployerProfile {
     
 }
